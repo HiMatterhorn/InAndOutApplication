d79a9e03d185ec94553b76f30655c0d4482c3c7e
HiMatterhorn <wyp4tomasz@gmail.com>
2021-12-19 18:19:19 +0100

    Update of Model - Lender and Item name attributes added

---
+++ InAndOut/Data/Migrations/20211219170921_addLenderAItemNameColumns.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class addLenderAItemNameColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Borrower",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemName",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lender",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemName",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Lender",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Borrower",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace InAndOut.Models
 {
@@ -10,7 +6,11 @@ namespace InAndOut.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        [Required]
+        public string ItemName { get; set; }
 
 
     }
