b7ccfc6276edcaafc1f69e9e88ee8b2a18a6dcd7
HiMatterhorn <wyp4tomasz@gmail.com>
2021-12-19 18:06:47 +0100

    Presenting the data from database on a website (html + bootstrap)

--- InAndOut/Controllers/ItemController.cs
+++ InAndOut/Controllers/ItemController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace InAndOut.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
         }
     }
 }
