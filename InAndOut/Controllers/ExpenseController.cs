c698d259d38ef8b60247badcb81142994b025760
HiMatterhorn <wyp4tomasz@gmail.com>
2021-12-19 20:41:14 +0100

    Challange - My expenses module

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,39 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View(objList);
+        }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //POST - Create
+        public IActionResult Create(Expense obj)
+        {
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return (RedirectToAction("Index"));
+        }
+    }
+}
---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense Name")]
+        public string ExpenseName { get; set; }
+        [Required]
+        [DisplayName("Amount of Money")]
+        public double AmountOfMoney { get; set; }
+    }
+
+}
